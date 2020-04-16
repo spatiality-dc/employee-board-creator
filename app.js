85a07c06b4c0e6185fdaa448197ad132d690bdcc
spatiality-dc <dan@dinosaurmedia.org>
2020-04-16 22:49:00 +1000

    added the guidelines back in for reference

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
 const render = require("./lib/htmlRenderer");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
 
 async function init() {
   const employeeArray = await createEmployeeList();
@@ -17,6 +18,9 @@ async function init() {
   const htmlData = render(employeeClassArray);
   renderHTML(htmlData);
 }
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
 
 async function createEmployeeList() {
   const teamArray = [];
@@ -97,6 +101,10 @@ function createEmployeeClass(teamArray) {
     console.log(teamArray);
   });
 }
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
 
 function renderHTML(htmlData) {
   fs.writeFileSync(outputPath, htmlData);
